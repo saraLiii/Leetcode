b988ef6ab1a3a0e45f35f6fca879968dd3d1fa26
saraLiii <byronsara9@gmail.com>
2026-01-11 00:41:24 +0800

    calculator_II

--- string/calculate.go
+++ string/calculate.go
@@ -169,3 +169,85 @@ func calculate(s string) (ans int) {
     }
     return
 }
+
+/*
+给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+
+整数除法仅保留整数部分。
+
+你可以假设给定的表达式总是有效的。所有中间结果将在 [-231, 231 - 1] 的范围内。
+
+注意：不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+
+ 
+
+示例 1：
+
+输入：s = "3+2*2"
+输出：7
+示例 2：
+
+输入：s = " 3/2 "
+输出：1
+示例 3：
+
+输入：s = " 3+5 / 2 "
+输出：5
+ 
+
+提示：
+
+1 <= s.length <= 3 * 105
+s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+s 表示一个 有效表达式
+表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+题目数据保证答案是一个 32-bit 整数
+*/
+
+/*
+好久没有一把过了，这个官方用的是一个数值的栈，每次在运算符号出现得时候进行运算，如果是+，- 就直接入栈，如果是*，/ 就和栈顶元素计算后再入栈
+最后把栈里面的元素加起来就行了
+但是我这样也行，这样主要是考虑到，前面的数字是可以算出结果的，只需要存之前的和现在的就行
+*/
+
+
+func calculate_II(s string) int {
+    ans,n,sign1,nums1:=0,len(s),1,0
+    sign2:=0
+    for i:=0;i<n;i++{
+        switch s[i]{
+            case ' ':
+                continue
+            case '-':
+                ans = ans + sign1*nums1
+                sign1= -1
+                nums1=0
+            case '+':
+                ans = ans + sign1*nums1
+                sign1= 1
+                nums1=0
+            case '*':
+                sign2=1
+            case '/':
+                sign2=-1
+            default:
+                nums2:=int(s[i]-'0')
+                i++
+                for ;i<n&&s[i]>='0'&&s[i]<='9';i++{
+                    nums2=nums2*10+ int(s[i]-'0')
+                }
+                i--
+                if sign2!=0{
+                    if sign2==1{
+                        nums2=nums1*nums2
+                    }else{
+                        nums2=nums1/nums2
+                    }
+                    sign2=0
+                }
+                nums1=nums2
+        }
+    }
+    return ans+nums1*sign1
+    
+}
